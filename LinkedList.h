132cee341bf4cabab0b7790c2b1ae63e128c5626
Eduardo <juantokx@gmail.com>
2025-10-29 12:07:42 -0600

    Implemented initial methods of LinkedList.tpp

--- LinkedList.h
+++ LinkedList.h
@@ -6,8 +6,25 @@
 template <typename T>
 class LinkedList {
 	private:
-        
+        LinkedListNode<T>* head;
+        unsigned int n;
     public:
+        LinkedList();
+        ~LinkedList();
+
+        unsigned int size() const;
+        bool empty() const;
+
+        void clear();
+        bool remove(T& value);
+
+        void pushFront(const T& value);
+        void pushBack(const T& value);
+        bool insert(unsigned int index, const T& value);
+
+        T* elementAt(unsigned int index);
+
+        void print() const;
 };
 
 
